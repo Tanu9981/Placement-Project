f418d894888322639f0dede29a6c29046e44ee77
tanu <tanuanjana111@gmail.com>
2026-04-14 02:08:21 +0530

    connected frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://placement-project-g1kt.onrender.com/"
 });
 
 // token auto attach
