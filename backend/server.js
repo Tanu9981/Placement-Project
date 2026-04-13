46e8175e62b0ec8963d70e6c6d55486aed4ab81a
tanu <tanuanjana111@gmail.com>
2026-04-14 02:32:22 +0530

    added cors fix

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -15,7 +16,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+   origin: "*"
+}));
 app.use(express.json());
 app.use("/api/company", companyRoutes);
 
